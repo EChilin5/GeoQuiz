ffc6997c9d9806b8d6db0e9e2723338f2c0473bb
EChilin5 <edgar3ac@gmail.com>
2021-01-31 23:12:08 -0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.bignerdranch.android.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    // lateinit is used to indicate to teh compiler that it is a non-null view
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0,0)
+            toast.show()
+        }
+
+        falseButton.setOnClickListener{ view:View->
+            val toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0,0)
+            toast.show()
+
+
+        }
+
+    }
+}
